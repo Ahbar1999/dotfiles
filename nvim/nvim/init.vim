01ebc7fd5fc40c846d53327c3c94f230aaac459c
Ahbar <ahbar1234@gmail.com>
2026-08-06 20:31:45 +0530

    (gopls) modified config

---
+++ nvim/nvim/init.vim
@@ -0,0 +1,188 @@
+" Use (:execute 'edit' luaFile) to edit the lua config
+" Moved lua import at the bottom because all the imports in lua need to be
+" after 'plug#end'
+" Set lua file's path according to OS filesystem
+let luaFile = "~/AppData/Local/nvim/lua/config/init.lua"
+
+if has('unix')
+	let luaFile = "~/.config/nvim/lua/config/init.lua"
+endif
+
+" Print some greeting messages 
+echo "(^ - ^)/"
+
+" Using multiline strings 
+echo "Hello, Ahbar\n\r"
+
+" just remove the fking eysore
+
+" \ ██╗░░░░░░░██╗███████╗██╗░░░░░░█████╗░░█████╗░███╗░░░███╗███████╗  ██████╗░░█████╗░░█████╗░██╗░░██╗██╗\n\r
+" \ ██║░░██╗░░██║██╔════╝██║░░░░░██╔══██╗██╔══██╗████╗░████║██╔════╝  ██╔══██╗██╔══██╗██╔══██╗██║░██╔╝██║\n\r
+" \ ╚██╗████╗██╔╝█████╗░░██║░░░░░██║░░╚═╝██║░░██║██╔████╔██║█████╗░░  ██████╦╝███████║██║░░╚═╝█████═╝░██║\n\r
+" \  ████╔═████║░██╔══╝░░██║░░░░░██║░░██╗██║░░██║██║╚██╔╝██║██╔══╝░░  ██╔══██╗██╔══██║██║░░██╗██╔═██╗░╚═╝\n\r
+" \  ╚██╔╝░╚██╔╝░███████╗███████╗╚█████╔╝╚█████╔╝██║░╚═╝░██║███████╗  ██████╦╝██║░░██║╚█████╔╝██║░╚██╗██╗\n\r
+" \   ╚═╝░░░╚═╝░░╚══════╝╚══════╝░╚════╝░░╚════╝░╚═╝░░░░░╚═╝╚══════╝  ╚═════╝░╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚═╝\n\r
+" \ \n\r\n\r
+" \ \n\r\n\r
+" \ \n\r\n\r
+" \ \n\r\n\r"
+
+" Setting some editor variables
+set number
+set relativenumber
+set ts=4
+set sw=4
+set autoindent
+" Mapping keyboard bindings
+
+" referenced by <leader>
+let mapleader = "-"
+" referenced by <localleader>
+let localleader = ","
+
+" Cut the current line and paste it below the current line, works in all the modes
+nnoremap <leader>- ddp
+" Same as above except moves the line upwards
+nnoremap <leader>_ ddkP
+" Dont use the above 2 commands at the first or last line, its not perfect ik 
+" Delete the current line in insertion mode(Although the exact steps are longer) 
+vnoremap <c-d> <esc>ddi 
+
+inoremap <leader>s <esc>:w<cr>
+" Open File Explorer with Ctrl+e
+nnoremap <c-e> :vsplit<cr><esc>:execute('Explore')<cr>
+" Open and edit $MYVIMRC file with keyboard shortcuts
+" '<cr>' is carriage return, here basically it means to execute the command
+" after entering it
+nnoremap <leader>ovr :e $MYVIMRC<cr>
+nnoremap <leader>ev  :vsplit $MYVIMRC<cr>
+nnoremap <leader>sv  :source $MYVIMRC<cr><cr>:e<cr>
+nnoremap <leader>olua :execute 'edit' luaFile<cr>
+
+" Split window vertically and open the command prompt in one buffer 
+nnoremap <leader>ot :vsplit<cr><esc>:terminal<cr>i 
+inoremap <leader>e  <esc>eli
+inoremap <leader>b  <esc>bi
+nnoremap <leader>pfn <esc>:echo expand('%:t')<cr>
+
+" File search shortcuts using fzf
+nnoremap <leader>F :Files<cr> 
+
+" visual mode -> go the other paranthese pair's start of line  
+nnoremap <leader>{ v%0
+
+" SOME AUTOCOMMAND SETTINGS
+" Comment Shortcuts
+augroup comment_group
+	autocmd FileType javascript nnoremap <buffer> <leader>cc I//<esc>
+	autocmd FileType python     nnoremap <buffer> <leader>cc I#<esc>
+	autocmd FileType python     nnoremap <buffer> <leader>cu I<esc>x<esc>
+	" Go to the start of the line and comment and uncomment it 
+	autocmd FileType rust	    nnoremap <buffer> <leader>cc I//<esc>
+	autocmd FileType rust 	    nnoremap <buffer> <leader>cu ^xxi<esc>
+augroup END
+
+
+" fn stores stores the filename without extensions
+let fn = expand('%:t:r')
+
+function SetKotlinState(filename)
+	echo "In a kotlin file"
+	let b:compileCmdDotKt = "!kotlinc"." ".a:filename.".kt"." "."-include-runtime -d"." ".a:filename.".jar"
+	let b:runCmdDotKt = "!java -jar"." ".a:filename.".jar"
+	nnoremap <buffer> <leader>cp :execute(b:compileCmdDotKt)<cr>
+	nnoremap <buffer> <leader>rp :execute(b:runCmdDotKt)<cr>
+endfunction
+
+
+augroup compile_run_group
+	" Run program shortcuts
+	" Generate commands from strings with execute/exec command and execute
+	" them all in one go
+	" exec introduces single whitespace be default between its arguments 
+	autocmd FileType python inoremap <buffer> <F5> <esc>:w<cr>:exec "!python" fn.".py"
+	autocmd FileType python nnoremap <buffer> <F5> :w<cr>:exec "!python" fn.".py"		
+	autocmd FileType rust nnoremap <buffer> <F5> :w<cr>:exec "!cargo run" fn.".rs"	
+	" Compile Shortcuts for Kotlin
+	autocmd FileType kotlin call SetKotlinState(fn)
+	" Compile Shortcuts for Scala
+	" scalac compiles and scala compiles and executes
+	autocmd FileType scala inoremap <buffer> <F5> <esc>:w<cr>:exec "!scala" fn.".scala"
+	autocmd FileType scala nnoremap <buffer> <F5> :w<cr>:exec "!scala" fn.".scala"
+	" autocmd BufWrite *.scala echo 'scala file written'
+	
+	" Compile/Build shortcuts for cpp
+	autocmd FileType cpp inoremap <buffer> <F5> <esc>:w<cr>:exec "!g++" fn.".cpp -o" fn	
+	autocmd FileType cpp nnoremap <buffer> <F5> :w<cr>:exec "!g++" fn.".cpp -o" fn
+augroup END
+
+"if expand('%:e') == 'kt' 
+"	call SetKotlinState(fn)
+" endif
+
+augroup abbreviations
+	autocmd FileType python iabbrev <buffer> iff if:<left>
+	autocmd FileType python	iabbrev <buffer> forloop for i in range(n):<cr>	
+	autocmd FileType python iabbrev <buffer> nameequalsmain if __name__ == '__main__':<cr>
+	autocmd FileType python iabbrev <buffer> nameismain if __name__ == '__main__':<cr>	
+	autocmd FileType javascript iabbrev <buffer> iff if ()<left>
+augroup END
+
+
+"START PLUG-IN MANAGER SETTINGS
+call plug#begin()
+
+" lsp config
+Plug 'neovim/nvim-lspconfig'
+" Plug 'neovim/nvim-lspconfig'
+" Make sure you use single quotes
+" Shorthand notation; fetches https://github.com/junegunn/vim-easy-align
+Plug 'junegunn/vim-easy-align'
+Plug 'vim-airline/vim-airline'
+Plug 'rust-lang/rust.vim'
+" Dracula color scheme
+Plug 'dracula/vim', { 'as': 'dracula' }
+" Gruvbox color scheme
+Plug 'morhetz/gruvbox'
+" Plug 'EdenEast/nightfox.nvim'
+
+" GOLANG Extension
+" Plug 'fatih/vim-go', {'do': ':GoUpdateBinaries' }
+" Plug 'https://github.com/golang/tools/tree/master/gopls'
+
+" kotlin
+Plug 'udalov/kotlin-vim'
+
+" RUST Plugins
+Plug 'rust-lang/rust.vim'
+
+" phpactor plugin(also implements language server)
+Plug 'phpactor/phpactor', {'for': 'php', 'tag': '*', 'do': 'composer install --no-dev -o'}
+
+" Fuzzy file finder
+Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
+Plug 'junegunn/fzf.vim'
+
+" Plug 'neoclide/coc.nvim', {'branch': 'release'}
+
+" tree sitter plugin
+Plug 'nvim-treesitter/nvim-treesitter', {'lazy': 'false', 'build': ':TSUpdate'}
+
+" Clojure repl plugin
+" Plug 'Olical/conjure'
+" Plug 'tpope/vim-dispatch'
+" Plug 'clojure-vim/vim-jack-in'
+" Plug 'radenling/vim-dispatch-neovim'
+
+call plug#end()
+"END PLUG-IN MANAGER SETTINGS
+
+" Load the lua files in config folder and execute them
+lua require('config')
+
+syntax enable
+" set background=dark
+" colorscheme gruvbox 
+" colorscheme carbonfox
+syntax enable
+filetype plugin indent on
---
+++ nvim/nvim/lua/config/init.lua
@@ -0,0 +1,216 @@
+-- nvim config main file 
+-- lets fucking learn lua and fuck around with vim no ?
+
+-- lsp_config = require('lspconfig') --> deprecated  
+-- migrate to vim.pack
+vim.pack.add({
+	{ src = 'https://github.com/EdenEast/nightfox.nvim', name = 'nightfox' },
+})
+
+-- print("loading lua configs")
+local opts = { noremap = true, silent = true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+-- contextual visual highlight 
+vim.keymap.set({'x'}, ']n', function() 
+	require 'vim.treesitter._select'.select_next(vim.v.count1)
+end, { noremap = true, desc = 'select next(or more if passed a count) treesitter node'})
+
+vim.keymap.set({'x'}, '[n', function() 
+	require 'vim.treesitter._select'.select_prev(vim.v.count1)
+end, { noremap = true, desc = 'select previous(or more if passed a count) treesitter node'})
+
+vim.keymap.set({'x', 'o'}, 'an', function() 
+	if vim.treesitter.get_parser(nil, nil, {error =false}) then
+		require 'vim.treesitter._select'.select_parent(vim.v.count1)
+	else
+		vim.lsp.buf.selection_range(vim.v.count1)
+	end
+end, { noremap = true, desc = 'select one parent(or more if passed a count) treesitter node; falls back to basic selection if treesitter not installed'})
+
+vim.keymap.set({'x', 'o'}, 'in', function() 
+	if vim.treesitter.get_parser(nil, nil, {error =false}) then
+		require 'vim.treesitter._select'.select_child(vim.v.count1)
+	else
+		vim.lsp.buf.selection_range(vim.v.count1)
+	end
+end, { noremap = true, desc = 'select one child(or more if passed a count) treesitter node; falls back to basic selection if treesitter not installed'})
+
+-- this is so the preview window, when autocomplete triggers, is not shown
+vim.api.nvim_set_option('completeopt', 'menu')
+-- print(vim.api.nvim_get_option('completeopt'))
+
+local on_attach = function(client, bufnr)
+	-- Enable compeletion triggered by <Ctrl-x><Ctrl-o>
+	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+	-- type hints
+	vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }	
+	vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+	vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+	vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+	vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+	vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+	vim.keymap.set('n', '<space>wl', function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, bufopts)
+	vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+	vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+	vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+	vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+	vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+end
+
+vim.lsp.config("pyright", {
+	on_attach = on_attach, 
+	flags = {}
+})
+
+vim.lsp.config("tsserver", {
+	on_attach = on_attach, 
+	flags = {}
+})
+
+vim.lsp.config("rust_analyzer", {
+	on_attach = on_attach, 
+	flags = {},
+	-- server specific settings	
+	settings = {
+		["rust-analyzer"] = {}
+	}
+})
+
+-- require("ccls").setup({lsp = {use_defaults = true}})
+vim.lsp.config("ccls", {
+	on_attach = on_attach,
+	flags = {},
+	root_markers = {"compile_commands.json", ".clangd", "./build/compile_commands.json", ".ccls", ".git"},
+	--- root_dir = vim.lsp.util.root_pattern('compile_commands.json', './build/compile_commands.json', '.ccls', '.git') -> deprecated
+})
+
+vim.lsp.config("gopls", {
+	on_attach = on_attach,
+	flags = {},
+	cmd = {"gopls", "serve"},	
+	filetypes = {"go", "gomod"},
+	root_markers = {"go.work", "go.mod", ".git"},
+	settings = {
+		gopls = {
+			analyses = {
+				unusedparams = true,
+			},
+			staticcheck = true,
+			gofumpt = true,
+		}
+	}
+})
+
+-- copied from https://go.dev/gopls/editor/vim 
+autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+    local params = vim.lsp.util.make_range_params()
+    params.context = {only = {"source.organizeImports"}}
+    -- buf_request_sync defaults to a 1000ms timeout. Depending on your
+    -- machine and codebase, you may want longer. Add an additional
+    -- argument after params if you find that you have to write the file
+    -- twice for changes to be saved.
+    -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+    for cid, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+          vim.lsp.util.apply_workspace_edit(r.edit, enc)
+        end
+      end
+    end
+    vim.lsp.buf.format({async = false})
+  end
+})
+
+vim.lsp.config("clangd", {
+	on_attach = on_attach,
+	cmd = {
+		'clangd', 
+		'--background-index',
+		'--clang-tidy', 
+		'--log=verbose',
+		'--query-driver=/usr/bin/g++,/usr/bin/clang++,/usr/bin/clang',
+		'--header-insertion=never',	-- what does it do ?? idk
+	},	
+	init_options = {
+		fallbackFlags = {'-std=c++17'},
+	},
+	root_markers = {"compile_commands.json", ".git"},
+	filetypes = {'c', 'cpp'},
+})
+
+-- enable lsp clients 
+vim.lsp.enable({"pyright"})
+vim.lsp.enable("tsserver")
+vim.lsp.enable("rust_analyzer")
+-- vim.lsp.enable({"ccls"})
+vim.lsp.enable({"gopls"})
+vim.lsp.enable({"clangd"})
+
+-- config treesitter(probably should start separating these blocks in separate files
+
+vim.api.nvim_create_autocmd('FileType', {
+	pattern = {'cpp', 'c', 'rust'},
+	callback = function()
+		vim.treesitter.start()
+		vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+		vim.opt.foldmethod = 'expr'
+		vim.opt.foldenable = false 
+		-- zo, zc, zO(open all), zC(close all), za, zv(view cursor line), zx(update fold), zm(fold more)
+		-- vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+		-- vim.wo[0][0].foldmethod = 'expr'
+	end,
+})
+
+-- carbonfox theme customization(yeah this is my life, i customize my text editor in my precious time
+require('nightfox').setup({
+	options= {
+		dim_inactive = false,
+		transparent= true,
+		styles = {
+			comments = "italic",
+			types = "bold"
+		},
+	},
+
+	-- groups = {
+		-- all = {
+		-- 	Visual = { bg = "palette.cyan.dim" },
+		-- },
+	-- },
+})
+
+-- following line doesnt seem to work, need to figure this out, 
+-- otherwise the theme just converts unused imports into comments visually
+-- vim.api.nvim_set_hl(0, "@keyword.import.cpp", {})
+
+-- treesitter config -> set in as autocmd
+-- vim.opt.foldmethod = 'expr'
+-- vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+
+-- colorscheme settings
+vim.cmd.colorscheme('carbonfox')
+
+border = string.rep("*", 10)
+empty_line = string.rep(" ", 10)
+--print("\n\n")
+print(border)
+print("Loaded lua.init")
+-- print("Dont' forget to add me")
+print(border)
+print(empty_line)
+--print("\n\n")
+
